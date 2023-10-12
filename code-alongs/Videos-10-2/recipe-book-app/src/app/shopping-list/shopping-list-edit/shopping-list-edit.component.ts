d1b9a5a334f2628d893e4c5fbd121bc8fec6337c
hilindst <114443718+hilindst@users.noreply.github.com>
2023-10-12 09:49:17 -0500

    Fixed add item error!

--- code-alongs/Videos-10-2/recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ code-alongs/Videos-10-2/recipe-book-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -8,8 +8,8 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('#nameInput') nameInputRef: ElementRef;
-  @ViewChild('#amountInput') amountInputRef: ElementRef;
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
 
   constructor(private slService: ShoppingListService) { }
