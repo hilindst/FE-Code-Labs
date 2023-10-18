4893cb9b7ab87eb433674191602dd357557bae75
hilindst <114443718+hilindst@users.noreply.github.com>
2023-10-18 09:49:27 -0500

    Adding Routing

--- code-alongs/Videos-10-2/recipe-book-app/src/app/shopping-list/shopping-list.service.ts
+++ code-alongs/Videos-10-2/recipe-book-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
 import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs-compat";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
       new Ingredient('Hams', 3),
       new Ingredient("Donuts", 17),
@@ -14,11 +15,11 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
-      this.ingredientsChanged.emit(this.ingredients.slice());
+      this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]){
      this.ingredients.push(...ingredients);
-     this.ingredientsChanged.emit(this.ingredients.slice());
+     this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
